71b54cb0483f8be4bdaf1282e80d95fe13c539fd
Darryl Lutchmipersad <Darryl.lutchmipersad@gmail.com>
2022-03-25 11:54:50 -0400

    Add counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter.component.ts';
+import { CounterComponent } from './counter.component';
 
 @NgModule({
   declarations: [
